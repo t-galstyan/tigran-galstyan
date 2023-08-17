987f3358064c00decf4bd8efa7fdb37ce576a507
Edgar-Nik <edgarnikogosian89@gmail.com>
2023-08-17 18:42:34 +0400

    image bg

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -17,76 +17,10 @@ const MainLayout = () => {
     }
   }, []);
 
-  // const videoElement = videoRef.current;
-
-  // if (videoElement) {
-  //   videoElement.addEventListener('loadeddata', (e) => {
-  //     //Video should now be loaded but we can add a second check
-
-  //     if (videoElement.readyState >= 3) {
-  //       console.log('ready');
-  //       setShowLoader(false);
-  //     }
-  //   });
-
-  //   videoElement.addEventListener('loadeddata', (e) => {
-  //     //Video should now be loaded but we can add a second check
-
-  //     if (videoElement.readyState >= 3) {
-  //       console.log('ready');
-  //       setShowLoader(false);
-  //     }
-  //   });
-  // }
-  const handleLoaded = (e) => {
-    console.log('loaded');
-    if (window.location.search === '?mode=dev') {
-      if (videoRef.current?.readyState >= 3) {
-        console.log('ready');
-        setShowLoader(false);
-      }
-    }
-  };
-
-  const handleEnded = (e) => {
-    console.log('ended');
-
-    if (window.location.search === '?mode=dev') {
-      videoRef.current?.play();
-      setShowLoader1(true);
-    }
-  };
-
   return (
     <div className="App">
-      {showLoader && (
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '300px',
-            background: 'red',
-            zIndex: 99999
-          }}
-        ></div>
-      )}
-      {showLoader1 && (
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '200px',
-            background: 'green',
-            zIndex: 9999999
-          }}
-        ></div>
-      )}
       <div className="main-layout">
-        <video
+        {/* <video
           ref={videoRef}
           className="video-bg"
           playsInline
@@ -97,7 +31,13 @@ const MainLayout = () => {
           onEnded={handleEnded}
         >
           <source src={require('../assets/video/bg.mp4')} type="video/mp4" />
-        </video>
+        </video> */}
+
+        <div class="galaxy-wrapper">
+          <div class="galaxy-image"></div>
+          <div class="galaxy-gradient"></div>
+        </div>
+
         <Header />
         <main className="main-content">
           <Outlet />
