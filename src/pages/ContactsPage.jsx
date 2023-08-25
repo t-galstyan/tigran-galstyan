bfe65f6adb9a4968655f43d64e5efe91b24223bc
Edgar-Nik <edgarnikogosian89@gmail.com>
2023-08-25 16:54:15 +0400

    Changed map

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -9,7 +9,7 @@ const ContactsPage = () => {
           <iframe
             className="map"
             title="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1527.663839989207!2d44.48584161923924!3d40.02346286900029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406ac858d4cf7e13%3A0x6aea452dd401213e!2sSundukyan%20St%2C%20Mkhchyan%200725!5e0!3m2!1sru!2sam!4v1692221576927!5m2!1sru!2sam"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3047.442897092394!2d44.49723177591206!3d40.199209968849694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abd15696fd471%3A0x1b9e50c3aefa70ba!2sGabriel%20Sundukyan%20St%2C%20Yerevan!5e0!3m2!1sru!2sam!4v1692965681965!5m2!1sru!2sam"
             width="600"
             height="450"
             style={{ border: 0 }}
@@ -17,6 +17,7 @@ const ContactsPage = () => {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
+
           <div className="text-container">
             <p>
               <b>Address: </b> apt 50, 90 Sundukyan str., Yerevan Armenia
